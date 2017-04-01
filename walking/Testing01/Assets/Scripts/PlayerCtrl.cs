bb6eff0c74ac3e7dd9ff43d735f6f14717c4c42b
Ta Thitiwat <Ta Thitiwat>
2017-04-01 22:53:39 +0700

    add unity app: test01

---
+++ walking/Testing01/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    //private Rigidbody2D myRigidBody;
+
+    
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+        facingRight = true;
+        //myRigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        HandleMovement(horizontal, vertical);
+
+        Flip(horizontal);
+	}
+
+    private void HandleMovement(float horizontal, float vertical) {
+        //myRigidBody.velocity = Vector2.left;
+        //myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
+        //transform.Translate(horizontal * movementSpeed, vertical * movementSpeed, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * movementSpeed * Time.deltaTime);
+        }
+        
+    }
+
+    private void Flip(float horizontal) {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+
+    }
+}
